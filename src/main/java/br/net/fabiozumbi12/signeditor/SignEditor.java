45734a33070e03aa3b93665cf2d58d6b4ceeeb1e
FabioZumbi12 <zumbi12doze@gmail.com>
2016-09-08 07:25:49 -0300

    First release.

---
+++ src/main/java/br/net/fabiozumbi12/signeditor/SignEditor.java
@@ -0,0 +1,167 @@
+package br.net.fabiozumbi12.signeditor;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.tileentity.TileEntity;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.source.ConsoleSource;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.data.manipulator.mutable.tileentity.SignData;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.block.InteractBlockEvent;
+import org.spongepowered.api.event.filter.cause.First;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.game.state.GameStoppingServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.serializer.TextSerializers;
+
+@Plugin(id = "br.net.fabiozumbi12.signeditor", 
+name = "SignEditor", 
+version = "1.0.0",
+authors="FabioZumbi12", 
+description="Simple tool to edit sign lines.")
+public class SignEditor {
+	
+	private HashMap<String, List<Text>> lines = new HashMap<String, List<Text>>();
+	private HashMap<String, SignData> signs = new HashMap<String, SignData>();
+	private HashMap<String, Integer> copies = new HashMap<String, Integer>();
+	private ConsoleSource logger;
+	
+	@Listener
+    public void onServerStart(GameStartedServerEvent event) {
+		this.logger = Sponge.getServer().getConsole();
+		
+		//setline
+		CommandSpec setline = CommandSpec.builder()
+			    .description(Text.of("Use to edit one line of a placed sign."))
+			    .permission("signeditor.setline")
+			    .arguments(GenericArguments.integer(Text.of("1-4")),GenericArguments.remainingJoinedStrings(Text.of("text (max. length 16)")))
+			    .executor(new CommandExecutor(){
+
+					@Override
+					public CommandResult execute(CommandSource src,
+							CommandContext args) throws CommandException {
+						if (src instanceof Player){
+							int line = args.<Integer>getOne("1-4").get();
+							String text = args.<String>getOne("text (max. length 16)").get();
+							if (line > 4 || line < 1){
+								src.sendMessage(toText("[SignEditor] &4The line need to be below 1-4."));
+								return CommandResult.empty();
+							}
+							if (text.length() > 16){
+								src.sendMessage(toText("[SignEditor] &4The max length allowed is 16 characters."));
+								return CommandResult.empty();
+							}
+							
+							List<Text> lineList = Arrays.asList(Text.of(),Text.of(),Text.of(),Text.of());
+							if (lines.containsKey(src.getName())){
+								lineList = lines.get(src.getName());
+							}
+							lineList.set(line-1, toText(text));							
+							lines.put(src.getName(), lineList);
+							src.sendMessage(toText("[SignEditor] &aLine '"+line+"' set to '"+text+"&a'. Right click on sign to paste!"));
+							return CommandResult.success();
+						} 
+						return CommandResult.empty();
+					}			    	
+			    })
+			    .build();
+		Sponge.getCommandManager().register(this, setline, "setline");
+		
+		//copy
+		CommandSpec copysign = CommandSpec.builder()
+			    .description(Text.of("Copy one sign text to other signs"))
+			    .permission("signeditor.copysign")
+			    .arguments(GenericArguments.optional(GenericArguments.integer(Text.of("copies"))))
+			    .executor(new CommandExecutor(){
+
+					@Override
+					public CommandResult execute(CommandSource src,
+							CommandContext args) throws CommandException {
+						if (src instanceof Player){
+							signs.put(src.getName(), null);
+							if (args.hasAny("copies") && args.<Integer>getOne("copies").get() > 1){
+								int cop = args.<Integer>getOne("copies").get();
+								copies.put(src.getName(), cop-1);
+								src.sendMessage(toText("[SignEditor] &aClick in one sign to copy the text. ("+cop+" copies)"));
+							} else {
+								src.sendMessage(toText("[SignEditor] &aClick in one sign to copy the text. (no copies)"));
+							}							
+							return CommandResult.success();
+						}
+						return CommandResult.empty();
+					}})
+			    .build();
+		Sponge.getCommandManager().register(this, copysign, "copysign");		
+		
+		//done
+		logger.sendMessage(toText("SignEditor: [&aSignEditor enabled!&r]"));
+	}
+	
+	@Listener
+	public void onStopServer(GameStoppingServerEvent e) {
+		logger.sendMessage(toText("SignEditor: [&4SignEditor disabled!&r]"));
+	}
+	
+	@Listener
+    public void onInteractBlock(InteractBlockEvent event, @First Player p) {
+		String type = event.getTargetBlock().getState().getType().getName();
+		if (type.endsWith("_sign")){
+			if (lines.containsKey(p.getName())){				
+				TileEntity signEntity = event.getTargetBlock().getLocation().get().getTileEntity().get();
+				SignData data = signEntity.get(SignData.class).get();
+				
+				int linex = -1;
+				List<Text> lineTexts = lines.get(p.getName());				
+				for (Text l:lineTexts){
+					linex++;
+					if (l.isEmpty()){
+						continue;
+					}
+					data.setElement(linex, l);
+				}
+				signEntity.offer(data);
+				lines.remove(p.getName());
+				event.setCancelled(true);
+			} else if (signs.containsKey(p.getName())){
+				if (signs.get(p.getName()) == null){
+					signs.put(p.getName(), event.getTargetBlock().getLocation().get().getTileEntity().get().get(SignData.class).get());
+					p.sendMessage(toText("[SignEditor] &aSign copied! Now click in other sign to paste the text."));
+				} else {
+					TileEntity signEntity = event.getTargetBlock().getLocation().get().getTileEntity().get();
+					signEntity.offer(signs.get(p.getName()));
+					
+					if (copies.containsKey(p.getName())){
+						int qtd = copies.get(p.getName());
+						if (qtd <= 1){
+							copies.remove(p.getName());
+						} else {
+							copies.put(p.getName(), qtd-1);
+						}	
+						p.sendMessage(toText("[SignEditor] &aText pasted with sucess! Still "+qtd+" copies to paste."));
+					} else {
+						signs.remove(p.getName());
+						p.sendMessage(toText("[SignEditor] &aText pasted with sucess! (no copies)"));
+					}
+					
+				}
+				event.setCancelled(true);
+			}
+		}
+		
+	}
+	
+	public static Text toText(String str){
+    	return TextSerializers.FORMATTING_CODE.deserialize(str);
+    }
+}
